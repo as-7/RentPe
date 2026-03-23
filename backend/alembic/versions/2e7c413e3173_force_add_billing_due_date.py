e3a9b86a083aabf0a6871f586b254f0dc124f320
Aakash Sehrawat <aakash17275@iiitd.ac.in>
2026-03-23 21:18:02 +0530

    Force add billing_due_date

---
+++ backend/alembic/versions/2e7c413e3173_force_add_billing_due_date.py
@@ -0,0 +1,28 @@
+"""force_add_billing_due_date
+
+Revision ID: 2e7c413e3173
+Revises: b67496eca362
+Create Date: 2026-03-23 21:17:11.185355
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2e7c413e3173'
+down_revision: Union[str, Sequence[str], None] = 'b67496eca362'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
